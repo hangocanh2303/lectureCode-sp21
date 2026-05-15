da68889e03d27b073cd84a20381e817019506196
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-15 13:20:30 +0700

    complete ExamPrepare disc3 week3

--- exercises/disc3/ExamPrepare.java
+++ exercises/disc3/ExamPrepare.java
@@ -1,3 +1,5 @@
+import jh61b.junit.In;
+
 import java.util.Arrays;
 
 public class ExamPrepare {
@@ -68,21 +70,63 @@ public class ExamPrepare {
         }
 
         public static void evenOdd(IntList lst) {
-            if (lst.rest == null) {
+            if (lst == null || lst.rest == null) {
                 return;
             }
-            IntList even = lst;
             IntList odd = lst.rest;
-            IntList oddHead = odd;
-            while (even.rest != null && odd.rest != null) {
-                even.rest = even.rest.rest;
+            IntList oddHead = lst.rest;
+            while (lst.rest != null && odd.rest != null) {
+                lst.rest = lst.rest.rest;
                 odd.rest = odd.rest.rest;
-                even = even.rest;
+                lst = lst.rest;
                 odd = odd.rest;
             }
-            even.rest = oddHead;
+            lst.rest = oddHead;
         }
     }
+
+
+//    3 Partition
+//    Implement partition, which takes in an IntList lst and an integer k, and destructively partitions lst into k IntLists such that each list has the following
+//    properties:
+//    1. It is the same length as the other lists. If this is not possible, i.e. lst cannot
+//    be equally partitioned, then the later lists should be one element smaller.
+//    For example, partitioning an IntList of length 25 with k = 3 would result in
+//    partitioned lists of lengths 9, 8, and 8.
+//    2. Its ordering is consistent with the ordering of lst, i.e. items in earlier in lst
+//    must precede items that are later.
+//    These lists should be put in an array of length k, and this array should be returned.
+//    For instance, if lst contains the elements 5, 4, 3, 2, 1, and k = 2, then a possible
+//    partition (note that there are many possible partitions), is putting elements 5, 3, 2
+//    at index 0, and elements 4, 1 at index 1.
+//    You may assume you have the access to the method reverse, which destructively reverses the ordering of a given IntList and returns a pointer to the reversed IntList.
+//    You may not create any IntList instances. You may not need all the lines.
+//    Hint: You may find the % operator helpful.
+    public static IntList[] partition(IntList lst, int k) {
+        IntList[] array = new IntList[k];
+        int index = 0;
+        IntList L = reverse(lst);
+        while (L != null) {
+            IntList prev = array[index];
+            IntList next = L.rest;
+
+            array[index] = L;
+            array[index].rest = prev;
+
+            L = next;
+            index = (index + 1) % k;
+        }
+        return array;
+    }
+
+
+
+
+    private static IntList reverse(IntList lst) {
+        return lst;
+    }
+
+
 
     public static void main(String[] args) {
         int[] LL = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0};

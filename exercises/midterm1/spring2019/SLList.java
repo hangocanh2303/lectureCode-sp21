df9958266a58a24cf18816f5630a307cccee374c
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-28 09:29:27 +0700

    complete spring2019 midterm1 problem 5

--- exercises/midterm1/spring2019/SLList.java
+++ exercises/midterm1/spring2019/SLList.java
@@ -2,12 +2,12 @@ package spring2019;
 
 /** An SLList is a list of integers, which hides the terrible truth
    * of the nakedness within. */
-public class SLList<Blorp> implements List61B<Blorp>  {
+public class SLList<T> implements List61B<T>  {
 	private class Node {
-		public Blorp item;
+		public T item;
 		public Node next;
 
-		public Node(Blorp i, Node n) {
+		public Node(T i, Node n) {
 			item = i;
 			next = n;
 		}
@@ -23,7 +23,7 @@ public class SLList<Blorp> implements List61B<Blorp>  {
 		size = 0;
 	}
 
-	public SLList(Blorp x) {
+	public SLList(T x) {
 		sentinel = new Node(null, null);
 		sentinel.next = new Node(x, null);
 		size = 1;
@@ -33,24 +33,24 @@ public class SLList<Blorp> implements List61B<Blorp>  {
 	  *  the list. If position is greater than the
 	  *  size of the list, inserts at the end instead.
 	  */
-	 public void insert(Blorp item, int position) {
-		 Node p = sentinel;
-		 while (position > 1 && p.next != null) {
-			 position--;
-			 p = p.next;
-		 }
-		 Node newNode = new Node(item, p.next);
-		 p.next = newNode;
-	 }
+//	 public void insert(T item, int position) {
+//		 Node p = sentinel;
+//		 while (position > 1 && p.next != null) {
+//			 position--;
+//			 p = p.next;
+//		 }
+//		 Node newNode = new Node(item, p.next);
+//		 p.next = newNode;
+//	 }
 
  	/** Adds x to the front of the list. */
- 	public void addFirst(Blorp x) {
+ 	public void addFirst(T x) {
  		sentinel.next = new Node(x, sentinel.next);
  		size = size + 1;
  	}
 
  	/** Adds x to the end of the list. */
- 	public void addLast(Blorp x) {
+ 	public void addLast(T x) {
  		size = size + 1; 		
 
  		Node p = sentinel;
@@ -64,7 +64,7 @@ public class SLList<Blorp> implements List61B<Blorp>  {
  	}
 
 	 /** Returns the first item in the list. */
-	 public Blorp getFirst() {
+	 public T getFirst() {
 		 return sentinel.next.item;
 	 }
 
@@ -80,17 +80,17 @@ public class SLList<Blorp> implements List61B<Blorp>  {
 	 }
 
 	 /** Returns last item */
-	 public Blorp getLast() {
+	 public T getLast() {
 		 Node back = getLastNode();
 		 return back.item;
 	 }
 
 	 /** Returns the ith item in the list. */
- 	public Blorp get(int i) {
+ 	public T get(int i) {
 		return get(i, sentinel.next);
 	}
 
-	private Blorp get(int i, Node p) {
+	private T get(int i, Node p) {
  		if (i == 0) {
  			return p.item;
 		}
@@ -103,7 +103,7 @@ public class SLList<Blorp> implements List61B<Blorp>  {
  	}
 
 	 /** Deletes and returns last item. */
-	 public Blorp removeLast() {
+	 public T removeLast() {
 		 Node back = getLastNode();
 		 if (back == sentinel) {
 			 return null;
@@ -127,6 +127,27 @@ public class SLList<Blorp> implements List61B<Blorp>  {
 	 		p = p.next;
 		}
 	}
+
+	/** Inserts item into given index. For example, if the list is currently
+	 * [0, 10, 20, 30], and we call insert(25, 3), the list will become
+	 * [0, 10, 20, 25, 30]. If we then call insert(-5, 0), the list will become
+	 * [-5, 0, 10, 20, 25, 30]. */
+	public void insert(T item, int index) {
+		if (index < 0 || index > size()) {
+			throw new IllegalArgumentException();
+		}
+		insert(item, index, sentinel);
+		size += 1;
+	}
+	private void insert(T item, int index, Node curr) {
+		if (index == 0) {
+            curr.next = new Node(item, curr.next);
+		} else {
+			Node nextNode = curr.next;
+			insert(item, index - 1, nextNode);
+		}
+	}
+
 
 	public static void main(String[] args) {
  		/* Creates a list of one integer, namely 10 */

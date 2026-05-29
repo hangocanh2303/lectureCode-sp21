7e8300eebba2adb5dfee80ed9c15756029f172bb
anhhn3 <anhhn3@pvcombank.com.vn>
2026-05-29 08:42:05 +0700

    midterm1 spring 2021: completed problem 1

--- exercises/midterm1/spring2021/CourseCatalog.java
+++ exercises/midterm1/spring2021/CourseCatalog.java
@@ -20,4 +20,16 @@ public class CourseCatalog {
             courses = newArr;
         }
     }
+
+//    d) (30 Points) The design of the classes above is poor. One issue is that the classes expose their instance
+//    variables as public. Instead, access to these variables should be private and the classes should provide
+//    methods that do semantically useful tasks, e.g. the CourseCatalog might have a boolean
+//    courseNumberExists(int i) method.
+//    Another significant issue is the poor choice of data structure, specifically int[] in both classes. The
+//    resulting code is awkward and overly verbose. As we said in the first lecture "Good programmers care
+//    about the data structures [in their code]." One better choice of data structure would have been a List.
+//    Give one reason why a List is better.
+//    List Advantage: courses operation simple, save full course instance list in Course catalog and CalStudent, courses size in List, don't need to track
+//    Note that later in 61B, we will learn about the Set, which is an even better data structure for this task
+//    than a List
 }
